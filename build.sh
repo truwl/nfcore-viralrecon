bcfaf7a0f20ce1f4a71bbe3f1e0e63c544434d08
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:13:18 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-viralrecon:2.4.1_0.1.0 -t truwl/nfcore-viralrecon:latest .
+docker push truwl/nfcore-viralrecon:2.4.1_0.1.0
+docker push truwl/nfcore-viralrecon:latest
+        
